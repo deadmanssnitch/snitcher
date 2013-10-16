a32dfaa9d63cbec70e793632ec017507b8609f09
Steve Richert <steve.richert@gmail.com>
2013-10-16 17:09:09 -0400

    Test the Snitcher.snitch method

--- spec/snitcher_spec.rb
+++ spec/snitcher_spec.rb
@@ -2,6 +2,36 @@ require "spec_helper"
 
 describe Snitcher do
   describe ".snitch" do
-    it "works"
+    let(:token) { SecureRandom.hex(5) }
+
+    before do
+      stub_request(:get, /nosnch\.in/)
+    end
+
+    it "pings DMS with the given token" do
+      Snitcher.snitch(token)
+
+      expect(a_request(:get, "https://nosnch.in/#{token}")).to have_been_made.once
+    end
+
+    context "when successful" do
+      before do
+        stub_request(:get, "https://nosnch.in/#{token}").to_return(status: 200)
+      end
+
+      it "returns true" do
+        expect(Snitcher.snitch(token)).to eq(true)
+      end
+    end
+
+    context "when unsuccessful" do
+      before do
+        stub_request(:get, "https://nosnch.in/#{token}").to_return(status: 404)
+      end
+
+      it "returns false" do
+        expect(Snitcher.snitch(token)).to eq(false)
+      end
+    end
   end
 end
