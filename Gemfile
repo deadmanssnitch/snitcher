c1454610e4d36b8b14a5d7155061775f2c5aedb4
Steve Richert <steve.richert@gmail.com>
2014-01-15 14:48:29 -0500

    Install Coveralls

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 gemspec
 
 group :test do
+  gem "coveralls", "~> 0.7", require: false
   gem "rspec", "~> 2.14"
   gem "webmock", "~> 1.17"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
-require "snitcher"
+if ENV["TRAVIS"]
+  require "coveralls"
+  Coveralls.wear!
+end
 
-require "bundler"
-Bundler.require(:test)
+require "snitcher"
 
 Dir[File.expand_path("../support/*.rb", __FILE__)].each { |f| require f }
