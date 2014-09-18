9bff38e367f5570a828ae5c1faade1fe3558b10b
Chris Gaffney <gaffneyc@gmail.com>
2014-09-17 18:31:44 -0700

    Add support for the message option.
    Dead Man's Snitch can include a message with a check-in. This adds
    support for this functionality.

--- lib/snitcher.rb
+++ lib/snitcher.rb
@@ -1,13 +1,23 @@
+require "uri"
 require "net/http"
 
 module Snitcher
   extend self
 
-  def snitch(token)
-    http = Net::HTTP.new("nosnch.in", 443)
-    http.use_ssl = true
+  def snitch(token, opts = {})
+    uri = URI.parse("https://nosnch.in/#{token}")
 
-    response = http.request(Net::HTTP::Get.new("/#{token}"))
-    response.is_a?(Net::HTTPSuccess)
+    opts = {
+      :use_ssl => uri.port == 443
+    }
+
+    Net::HTTP.start(uri.host, uri.port, opts) do |http|
+      if message = opts[:message]
+        uri.query = URI.encode_www_form(:m => message)
+      end
+
+      response = http.request(Net::HTTP::Get.new(uri.request_uri))
+      response.is_a?(Net::HTTPSuccess)
+    end
   end
 end
