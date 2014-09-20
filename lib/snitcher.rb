5074d8a3d3fe741cd3bf59e6d2c1ff9af5a2852a
Chris Gaffney <gaffneyc@gmail.com>
2014-09-20 16:34:05 -0700

    Add documentation for `Snitch.snitch`.
    Using tomdoc (http://tomdoc.org/) for the documentation format since
    it's pretty clean and informative enough.

--- lib/snitcher.rb
+++ lib/snitcher.rb
@@ -5,6 +5,19 @@ require "net/http"
 module Snitcher
   extend self
 
+  # Public: Check-in to Deadman's Snitch
+  #
+  # token - The Snitch token given by Deadman's Snitch (see the install page).
+  # opts  - The hash of optional parameters that can be given during check-in:
+  #         :message - Text message limited to ~250 characters.
+  #         :timeout - Number of seconds to set as connect and read timeout.
+  #
+  # Examples
+  #
+  #   Snitch.snitch("c2354d53d2")
+  #   # => true
+  #
+  # Returns true if the check-in succeeded or false if it failed
   def snitch(token, opts = {})
     uri     = URI.parse("https://nosnch.in/#{token}")
     timeout = opts.fetch(:timeout, 2)
